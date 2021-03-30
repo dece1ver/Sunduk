57f941b8977a759f967c94681b6235cb4a076898
Dece1ver <dece1ver@pm.me>
2021-03-30 10:16:10 +0300

    чуть поправил резьбу

--- Sunduk.PWA/Program.cs
+++ Sunduk.PWA/Program.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using MudBlazor.Services;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Sunduk.PWA
 {
