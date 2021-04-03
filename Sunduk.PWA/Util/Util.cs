8ae72e0c2de8af994b2a49d0c73e7b5ecd30d172
Vladimir Gromov <v.dece1verr@hotmail.com>
2021-04-03 21:09:28 +0300

    отрефакторил всякую хрень, вынес общие методы в статик класс, добавил скругления, сменил qr

---
+++ Sunduk.PWA/Util/Util.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sunduk.PWA.Util
+{
+    public static class Util
+    {
+        public static double GetDouble(string stringNumber, double defaultValue = 0)
+        {
+            NumberFormatInfo numberFomat = new() { NumberDecimalSeparator = "," };
+            if (Double.TryParse(stringNumber, NumberStyles.Any, numberFomat, out double result))
+            {
+                return result;
+            }
+            return defaultValue;
+        }
+        public static int GetInt(string stringNumber, int defaultValue = 0)
+        {
+            NumberFormatInfo numberFomat = new() { NumberDecimalSeparator = "," };
+            if (Int32.TryParse(stringNumber, NumberStyles.Any, numberFomat, out int result))
+            {
+                return result;
+            }
+            return defaultValue;
+        }
+
+        public static double Degrees(double radians)
+        {
+            return radians * 180 / Math.PI;
+        }
+
+        public static double Radians(double degrees)
+        {
+            return degrees * Math.PI / 180;
+        }
+
+        public static string NCFormat(double value, int precision = 3)
+        {
+            return value.ToString($"F{precision}").Replace(",", ".").TrimEnd('0');
+        }
+
+        public static string ToPrettyString(double value, int precision = 3)
+        {
+            return value.ToString($"F{precision}").Replace(",", ".").TrimEnd('0').TrimEnd('.');
+        }
+    }
+
+    
+}
