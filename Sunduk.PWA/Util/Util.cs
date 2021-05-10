d040fb0c83f6d51d9f6e39e5029d15c393d4ff11
Vladimir Gromov <v.dece1verr@hotmail.com>
2021-05-10 23:20:07 +0300

    начал переписывать резьбу, добавил шаблоны, расчет высоты что-то еще

--- Sunduk.PWA/Util/Util.cs
+++ Sunduk.PWA/Util/Util.cs
@@ -5,6 +5,8 @@ namespace Sunduk.PWA.Util
 {
     public static class Util
     {
+        public enum PassesOption {FullPasses, Infeed }
+
         public static double GetDouble(string stringNumber, double defaultValue = 0)
         {
             NumberFormatInfo numberFomat = new() { NumberDecimalSeparator = "," };
@@ -42,6 +44,37 @@ namespace Sunduk.PWA.Util
         public static string ToPrettyString(double value, int precision = 3)
         {
             return value.ToString($"F{precision}").Replace(",", ".").TrimEnd('0').TrimEnd('.');
+        }
+
+        public static double[] CalcPasses(double threadDepth, int passesCount, PassesOption passesOption = PassesOption.FullPasses)
+        {
+            double[] passes = new double[passesCount];
+            for (int pass = 1; pass <= passesCount; pass++)
+            {
+                passes[pass - 1] = Math.Round(threadDepth / Math.Sqrt(passesCount - 1) * Math.Sqrt(pass > 1 ? pass - 1 : 0.3), 2);
+            }
+            if(passesOption == PassesOption.FullPasses)
+            {
+                return passes;
+            }
+            else
+            {
+                double[] infeed = new double[passesCount];
+                for (int i = 0; i < passes.Length; i++)
+                {
+                    
+                    if (i > 0)
+                    {
+                        infeed[i] = passes[i] - passes[i - 1];
+                    }
+                    else
+                    {
+                        infeed[i] = passes[i];
+                    }
+                }
+                return infeed;
+            }
+            
         }
     }
 
