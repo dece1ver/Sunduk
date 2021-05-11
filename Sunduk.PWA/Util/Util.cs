514e9e1b071c6857847ee4632c073513db1eed1d
Dece1ver <dece1ver@pm.me>
2021-05-11 16:24:43 +0300

    дописал резьбу, шаблоны, расчеты и все такое

--- Sunduk.PWA/Util/Util.cs
+++ Sunduk.PWA/Util/Util.cs
@@ -5,7 +5,7 @@ namespace Sunduk.PWA.Util
 {
     public static class Util
     {
-        public enum PassesOption {FullPasses, Infeed }
+        public enum PassesOption { FullPasses, Infeed }
 
         public static double GetDouble(string stringNumber, double defaultValue = 0)
         {
@@ -53,7 +53,7 @@ namespace Sunduk.PWA.Util
             {
                 passes[pass - 1] = Math.Round(threadDepth / Math.Sqrt(passesCount - 1) * Math.Sqrt(pass > 1 ? pass - 1 : 0.3), 2);
             }
-            if(passesOption == PassesOption.FullPasses)
+            if (passesOption == PassesOption.FullPasses)
             {
                 return passes;
             }
@@ -62,7 +62,7 @@ namespace Sunduk.PWA.Util
                 double[] infeed = new double[passesCount];
                 for (int i = 0; i < passes.Length; i++)
                 {
-                    
+
                     if (i > 0)
                     {
                         infeed[i] = passes[i] - passes[i - 1];
@@ -74,7 +74,7 @@ namespace Sunduk.PWA.Util
                 }
                 return infeed;
             }
-            
+
         }
     }
 
