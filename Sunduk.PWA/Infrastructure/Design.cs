135da55c25114a67ca059fd550c9f4605da99ee1
Vladimir Gromov <v.dece1verr@hotmail.com>
2021-12-22 15:42:00 +0300

    дизайн ковырнул чутка

---
+++ Sunduk.PWA/Infrastructure/Design.cs
@@ -0,0 +1,27 @@
+﻿using MudBlazor;
+
+namespace Sunduk.PWA.Infrastructure
+{
+    public static class Design
+    {
+        public static string GetFontSize(this Breakpoint breakpoint)
+        {
+            switch (breakpoint)
+            {
+                case Breakpoint.Xs: return "xx-small";
+                case Breakpoint.Sm: return "x-small";
+                default: return "medium";
+            }
+        }
+
+        public static string GetPaddingRight(this Breakpoint breakpoint)
+        {
+            switch (breakpoint)
+            {
+                case Breakpoint.Xs: return "10px";
+                case Breakpoint.Sm: return "15px";
+                default: return "25px";
+            }
+        }
+    }
+}
