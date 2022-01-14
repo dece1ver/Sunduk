d9ebd50529eff50c08b008f6a73196161cfb389f
Vladimir Gromov <v.dece1verr@hotmail.com>
2022-01-14 19:59:56 +0300

    апи для отправки сообщений

---
+++ Sunduk.WebApi/Program.cs
@@ -0,0 +1,25 @@
+using Sunduk.WebApi;
+using System.Net.Mail;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddSingleton<SendMessageService>(new SendMessageService());
+var feedbackFrom = builder.Configuration["FeedbackFrom"];
+var feedbackTo = builder.Configuration["FeedbackTo"];
+var feedbackPass = builder.Configuration["FeedbackPass"];
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/sendmessage|{Name}|{Message}", (string Name, string Message) =>
+{
+    SendMessageService.Send(Name, Message, feedbackFrom, feedbackTo, feedbackPass);
+});
+
+app.Run();
---
+++ Sunduk.WebApi/SendMessageService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Mail;
+
+namespace Sunduk.WebApi
+{
+    public class SendMessageService
+    {
+        public static bool Send(string Name, string Message, string From, string To, string Pass)
+        {
+            var result = false;
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(From);
+                mail.To.Add(To);
+                mail.Subject = $"Обратная связь от: {Name}";
+                mail.Body = Message;
+                mail.IsBodyHtml = false;
+
+                using (SmtpClient smtp = new SmtpClient("smtp.office365.com", 587))
+                {
+                    smtp.Credentials = new System.Net.NetworkCredential(From, Pass);
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
