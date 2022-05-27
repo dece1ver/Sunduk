2ef9fc87269caddac586ed5f1f09e8256e98d84f
Vladimir Gromov <v.dece1verr@hotmail.com>
2022-05-28 01:01:01 +0300

    интерфейс в контуре, скругление в начале контура

--- Sunduk.PWA/Infrastructure/Sequences/ContourElements/Point.cs
+++ Sunduk.PWA/Infrastructure/Sequences/ContourElements/Point.cs
@@ -4,13 +4,15 @@ namespace Sunduk.PWA.Infrastructure.Sequences.ContourElements
 {
     public class Point : Element
     {
-        public Point(double? x, double? z)
+        public Point(double? x, double? z, double blunt = 0)
         {
             X = x;
             Z = z;
+            Blunt = blunt;
         }
 
         public override double? X { get; set; }
         public override double? Z { get; set; }
+        public double Blunt { get; set; }
     }
 }
