d0ef0dd0b44838752fe88c3711c0d398ba01a844
dece1ver <v.dece1verr@gmail.com>
2025-07-22 22:45:31 +0300

    обновил до .net9, все сломалось, частично починил

---
+++ Sunduk.PWA/Infrastructure/DynamicTemplatesData/TemplateDefinition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Sunduk.PWA.Infrastructure.DynamicTemplatesData
+{
+    public class TemplateDefinition
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public List<VariableDefinition> Variables { get; set; } = new();
+    }
+}
---
+++ Sunduk.PWA/Infrastructure/DynamicTemplatesData/VariableDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace Sunduk.PWA.Infrastructure.DynamicTemplatesData
+{
+    public class VariableDefinition
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Expression { get; set; } = string.Empty;
+    }
+}
