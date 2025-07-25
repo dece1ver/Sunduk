ba5870bf1c4ab95af0a98d3a7d7409fc0c6460a8
Vladimir Gromov <v.dece1verr@gmail.com>
2025-07-25 13:25:58 +0300

    загрузки файлов

---
+++ Sunduk.PWA/Infrastructure/Extensions/TaskExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sunduk.PWA.Infrastructure.Extensions
+{
+    public static class TaskExtensions
+    {
+        /// <summary>
+         /// Запускает задачу без ожидания и подавляет предупреждение компилятора.
+         /// Можно передать необязательный обработчик ошибок.
+         /// </summary>
+        public static void AndForget(this Task task, Action<Exception>? onError = null)
+        {
+            _ = ForgetInternal(task, onError);
+        }
+
+        private static async Task ForgetInternal(Task task, Action<Exception>? onError)
+        {
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                onError?.Invoke(ex);
+            }
+        }
+    }
+}
